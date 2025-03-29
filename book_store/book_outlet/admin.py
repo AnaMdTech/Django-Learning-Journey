c8bc1118a75922d23b19246d378ed18035f7ed37
Anamd-hub <anasmdl120@gmail.com>
2025-03-29 23:22:40 +0300

    More Config Options

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -7,5 +7,8 @@ from.models import Book
 class BookAdmin(admin.ModelAdmin):
   # readonly_fields = ("slug",)
   prepopulated_fields = {"slug": ("title",)}
+  list_filter = ("author", "rating")
+  list_display = ("title", "author", "rating", "is_bestselling")
+  search_fields = ["title", "author", "rating"]
 
 admin.site.register(Book, BookAdmin)
