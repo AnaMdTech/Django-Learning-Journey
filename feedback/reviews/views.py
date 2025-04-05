1e22eca0bd3078813f964395f96d193d2bea8db2
Anamd-hub <anasmdl120@gmail.com>
2025-04-05 17:18:21 +0300

    Adding csrf protection & Handling Form Submission & Extracting Data

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 def review(request):
+  if request.method == 'POST':
+    # Process the form data here
+    entered_username = request.POST['username']
+    print(entered_username)
+    return HttpResponseRedirect('/thank-you')
+  
   return render(request, 'reviews/review.html')
+
+def thank_you(request):
+  return render(request, 'reviews/thank_you.html')
