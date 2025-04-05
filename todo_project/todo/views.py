3921febe7861078424097882e01deab8f0f712a6
Anamd-hub <anasmdl120@gmail.com>
2025-04-06 00:27:08 +0300

    Handle displaying and submitting tasks

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
     tasks = Task.objects.all()
-    return render(request, 'todo/task_list.html', {'tasks': tasks})
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'todo/task_list.html', context)
