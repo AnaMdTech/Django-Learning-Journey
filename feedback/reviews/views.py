b7bc037b794079953710b9c657de65da35c4704f
Anamd-hub <anasmdl120@gmail.com>
2025-04-08 22:47:47 +0300

    Replacing/Using Class View

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,21 +1,34 @@
 # from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.views import View
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
+# class based view
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, "reviews/review.html", {"form": form})
+    
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("thank-you")
 
-def review(request):
-    form = ReviewForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect("thank-you")
+#! function based view
+# def review(request):
+#     form = ReviewForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         return redirect("thank-you")
 
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+#     return render(request, "reviews/review.html", {
+#         "form": form
+#     })
 
 
 def thank_you(request):
