1b9d9fc33ee19f3ec81afec8dede9a77e8111de6
Anamd-hub <anasmdl120@gmail.com>
2025-04-09 23:05:13 +0300

    Updating Products using UpdateView

--- inventory_man't_system/invProject/invApp/views.py
+++ inventory_man't_system/invProject/invApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import ProductForm
 from .models import Product
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -24,13 +26,19 @@ def product_list_view(request):
     return render(request, 'invApp/product_list.html', {'products': products})
 
 # Update View
-def product_update_view(request, product_id):
-    product = Product.objects.get(product_id=product_id)
-    form = ProductForm(request.POST or None, instance=product)
-    if form.is_valid():
-        form.save()
-        return redirect('product_list')
-    return render(request, 'invApp/product_form.html', {'form': form})
+# def product_update_view(request, product_id):
+#     product = Product.objects.get(product_id=product_id)
+#     form = ProductForm(request.POST or None, instance=product)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('product_list')
+#     return render(request, 'invApp/product_form.html', {'form': form})
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'invApp/product_form.html'
+    success_url = reverse_lazy('product_list')
 
 # Delete View
 def product_delete_view(request, product_id):
