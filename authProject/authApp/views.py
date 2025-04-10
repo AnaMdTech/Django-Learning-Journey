dad97ba2f0e8cdff027e106ab4fd935c1d9f1661
Anamd-hub <anasmdl120@gmail.com>
2025-04-10 17:40:49 +0300

    Add necessery packages and classes for the app

--- authProject/authApp/views.py
+++ authProject/authApp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.views import View
+from .forms import RegisterForm
 
 # Create your views here.
+def register_view(request):
+  pass
+
+def login_view(request):
+  pass
+
+def logout_view(request):
+  pass
+
+# Home View
+# using decorators
+@login_required
+def home_view(request):
+  pass
