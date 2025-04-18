8db429c3f64fdc1e8cc677c20d97fcd9074bbc28
Anamd-hub <anasmdl120@gmail.com>
2025-04-18 18:16:06 +0300

    Add views and URL routing for home, about, and dynamic greet page

--- 03-django-templates/main/views.py
+++ 03-django-templates/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def greet(request, name):
+    return render(request, 'greet.html', {'name': name})
