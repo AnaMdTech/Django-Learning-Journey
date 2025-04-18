b68341f4630566fa8ff1e18191a97e2f7da0697e
Anamd-hub <anasmdl120@gmail.com>
2025-04-18 17:50:05 +0300

    Add URL pattern for dynamic greeting route

--- 02-django-views-routing/main/urls.py
+++ 02-django-views-routing/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('hello/<str:name>/', views.greet_user, name='greet_user'),
 ]
