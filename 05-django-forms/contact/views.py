8e1742c0311b6650f9d2e10a4bc54c35e101ccbd
Anamd-hub <anasmdl120@gmail.com>
2025-04-19 22:57:03 +0300

    Add contact view to handle form display and submission

--- 05-django-forms/contact/views.py
+++ 05-django-forms/contact/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
-# Create your views here.
+def contact_view(request):
+    submitted = False
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Normally you’d process/store/send data here
+            submitted = True
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/contact.html', {
+        'form': form,
+        'submitted': submitted
+    })
