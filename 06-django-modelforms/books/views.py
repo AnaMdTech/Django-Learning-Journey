62d013a9c6ef82a73f3ffa04d4775d6b6d7e9c52
Anamd-hub <anasmdl120@gmail.com>
2025-04-19 23:42:26 +0300

    Create templates for adding a book and displaying the book list

--- 06-django-modelforms/books/views.py
+++ 06-django-modelforms/books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import BookForm
+from .models import Book
 
 def add_book(request):
     if request.method == 'POST':
@@ -11,3 +12,7 @@ def add_book(request):
         form = BookForm()
 
     return render(request, 'books/add_book.html', {'form': form})
+
+def book_list(request):
+    books = Book.objects.all() # Get all books from the database
+    return render(request, 'books/book_list.html', {'books': books})
