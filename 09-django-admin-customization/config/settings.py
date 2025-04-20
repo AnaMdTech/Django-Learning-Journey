fa7b9ac2e23b8b3a21aae2fd688647dde1ceff71
Anamd-hub <anasmdl120@gmail.com>
2025-04-21 01:25:47 +0300

    Install and configure django-admin-interface and colorfield

--- 09-django-admin-customization/config/settings.py
+++ 09-django-admin-customization/config/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+X_FRAME_OPTIONS = "SAMEORIGIN"
