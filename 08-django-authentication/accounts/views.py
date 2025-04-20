9685031fa4a54b20968c0b33ccce06b692836290
Anamd-hub <anasmdl120@gmail.com>
2025-04-21 00:02:43 +0300

    Add user registration page using Django's UserCreationForm

--- 08-django-authentication/accounts/views.py
+++ 08-django-authentication/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration successful. You can now log in.")
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
