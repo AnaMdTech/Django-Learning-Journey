6b5b8bd4baa95afa55a101e630a68a42fc4d37ff
Anamd-hub <anasmdl120@gmail.com>
2025-04-21 17:42:06 +0300

    Creating and Registering Admin Models

--- 10-django-university_management-project/staff_management/admin.py
+++ 10-django-university_management-project/staff_management/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Department, Role, Staff
 
-# Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'department', 'role', 'salary', 'hire_date')
+    list_filter = ('department', 'role')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_per_page = 10
